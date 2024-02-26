ccb51bef63352cb0800acdc8b3c4fbcd37b6eaa4
mf-wear <michael.fung@wear-nft.com>
2024-02-26 21:32:11 +0800

    chore: add subItems (not in use)

--- packages/core/src/app/order/removeBundledItems.ts
+++ packages/core/src/app/order/removeBundledItems.ts
@@ -1,9 +1,10 @@
-import { LineItemMap } from '@bigcommerce/checkout-sdk';
+import { LineItemMap, PhysicalItem } from '@bigcommerce/checkout-sdk';
 
-export default function removeBundledItems(lineItems: LineItemMap): LineItemMap {
+export default function removeBundledItems(lineItems: LineItemMap): LineItemMap & { subItems?: PhysicalItem[] } {
     return {
         ...lineItems,
         physicalItems: lineItems.physicalItems.filter((item) => typeof item.parentId !== 'string'),
+        subItems: lineItems.physicalItems.filter((item) => typeof item.parentId === 'string'),
         digitalItems: lineItems.digitalItems.filter((item) => typeof item.parentId !== 'string'),
     };
 }
