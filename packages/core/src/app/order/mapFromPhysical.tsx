5ea80cf3ad330c383d42f2237168ab8fe5b70855
mf-wear <michael.fung@wear-nft.com>
2024-02-26 21:33:14 +0800

    chore: add color, change productOptions -> content to item.name

--- packages/core/src/app/order/mapFromPhysical.tsx
+++ packages/core/src/app/order/mapFromPhysical.tsx
@@ -4,19 +4,24 @@ import getOrderSummaryItemImage from './getOrderSummaryItemImage';
 import { OrderSummaryItemProps } from './OrderSummaryItem';
 
 function mapFromPhysical(item: PhysicalItem): OrderSummaryItemProps {
-    return {
-        id: item.id,
-        quantity: item.quantity,
-        amount: item.extendedComparisonPrice,
-        amountAfterDiscount: item.extendedSalePrice,
-        name: item.name,
-        image: getOrderSummaryItemImage(item),
-        description: item.giftWrapping ? item.giftWrapping.name : undefined,
-        productOptions: (item.options || []).map((option) => ({
-            testId: 'cart-item-product-option',
-            content: `${option.name} ${option.value}`,
-        })),
-    };
+  const [name, color] = item.name.split('|');
+
+  return {
+    id: item.id,
+    quantity: item.quantity,
+    amount: item.extendedComparisonPrice,
+    amountAfterDiscount: item.extendedSalePrice,
+    name,
+    color,
+    image: getOrderSummaryItemImage(item),
+    description: item.giftWrapping ? item.giftWrapping.name : undefined,
+    productOptions: (item.options || []).filter((item) => item.name !== '').map((item) => (
+      {
+        testId: 'cart-item-product-option',
+        content: item.name,
+      }
+    )),
+  };
 }
 
 export default mapFromPhysical;
